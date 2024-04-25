c2cd95e641de3c73fdd5ea7b27e6c06f9d9369fe
DJPing <136559594+pingwin840@users.noreply.github.com>
2024-04-25 20:41:00 +0300

    Update installrequirements.py

--- installrequirements.py
+++ installrequirements.py
@@ -1,9 +1,3 @@
 from sys import executable, platform
 import subprocess
-
-
-if platform == "win32":
-    subprocess.run([executable, "-m", "pip", "install", "-r", "requirements.txt"])
-
-if platform.startswith("linux"):
-    subprocess.run([executable, "-m", "pip", "install", "-r", "requirements.txt"])
+subprocess.run([executable, "-m", "pip", "install", "-r", "requirements.txt"])
